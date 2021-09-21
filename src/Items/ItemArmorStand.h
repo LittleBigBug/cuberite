4a6e355859e5fdba9011d797be9dfa6df1a73527
Ethan Jones <ethan@yasfu.net>
2021-09-21 08:47:48 -0600

    Armor stand Entity/block checks

--- src/Items/ItemArmorStand.h
+++ src/Items/ItemArmorStand.h
@@ -29,17 +29,39 @@ public:
 		eBlockFace a_ClickedBlockFace
 	) override
 	{
-		// TODO: Should verify that there are no armor stand or other mob before placing
-
 		if (a_ClickedBlockFace < 0)
 		{
 			return false;
 		}
 
 		const auto PlacePos = AddFaceDirection(a_ClickedBlockPos, a_ClickedBlockFace);
+		const auto AbovePos = AddFaceDirection(PlacePos, BLOCK_FACE_YP);
 		BLOCKTYPE PlaceBlock = a_World->GetBlock(PlacePos);
+		BLOCKTYPE AboveBlock = a_World->GetBlock(AbovePos);
 
-		if (PlaceBlock != E_BLOCK_AIR)
+		LOGD("%i %i %i", PlacePos.x, PlacePos.y, PlacePos.z);
+		LOGD("%i %i %i", AbovePos.x, AbovePos.y, AbovePos.z);
+
+		if (PlaceBlock != E_BLOCK_AIR || (AboveBlock != E_BLOCK_AIR))
+		{
+			return false;
+		}
+
+		const cBoundingBox BoundingBox(PlacePos, 0.4, 2);
+
+		bool FoundEntity = false;
+
+		a_World->ForEachEntityInBox(BoundingBox, [&FoundEntity](cEntity & ent)
+		{
+			if (ent.IsArmorStand() || ent.IsPawn() || ent.IsEnderCrystal())
+			{
+				FoundEntity = true;
+			}
+
+			return false;
+		});
+
+		if (FoundEntity)
 		{
 			return false;
 		}
